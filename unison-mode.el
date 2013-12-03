b88af17a68dca562f73973e87433d4184c422e8c
Karl F <karlfogel@gmail.com>
2013-12-03 22:27:49 +0100

    an example-file, and a screenshot of it

--- unison-mode.el
+++ unison-mode.el
@@ -3,7 +3,9 @@
 ;; Copyright (C) 2013  Karl Fogelmark
 
 ;; Author: Karl Fogelmark <karlfogel@gmail.com>
-;; Keywords:
+;; URL: https://github.com/impaktor/unison-mode
+;; Version: 0.1
+;; Keywords: symchronization, unison
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -19,8 +21,6 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;; Latest versio available at https://github.com/impaktor/unison-mode
-
 ;; Install by putting this file in ~/.emacs.d/elisp/ and this code in your
 ;; emacs configuration (~/.emacs):
 ;;
